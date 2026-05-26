6b300ee737d15a4f8845d76380731b607fbd4412
ahengelhard <adam.h.engelhard@gmail.com>
2026-05-26 17:06:42 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -205,4 +205,4 @@ if __name__ == "__main__":
     print("  SCREENPLAY COVERAGE TOOL")
     print("  Open your browser to:  http://localhost:5000")
     print("="*55 + "\n")
-    app.run(debug=False, port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
